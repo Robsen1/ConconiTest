73e2ce2b45e547ace5be70a2c5e3ddde15337444
Robert Gstöttner <robert-gstoettner@gmx.at>
2015-06-09 23:52:05 +0200

    setup1

---
+++ Conconii/app/src/main/java/at/fhooe/mc/conconii/ConconiMain.java
@@ -0,0 +1,14 @@
+package at.fhooe.mc.conconii;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+public class ConconiMain extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
