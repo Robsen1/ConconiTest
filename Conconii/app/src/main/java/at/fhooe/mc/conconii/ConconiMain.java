5001e9516b4bcd30c0b8a4ab408544093627d34a
Robert Gstöttner <robert-gstoettner@gmx.at>
2015-06-10 14:17:49 +0200

    INITIAL COMMIT EMPTY PROJECT

--- Conconii/app/src/main/java/at/fhooe/mc/conconii/ConconiMain.java
+++ Conconii/app/src/main/java/at/fhooe/mc/conconii/ConconiMain.java
@@ -1,17 +1,15 @@
 package at.fhooe.mc.conconii;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
 
-public class ConconiMain extends ActionBarActivity {
+public class ConconiMain extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // DAS IST EIN TEST
 
     }
 }
