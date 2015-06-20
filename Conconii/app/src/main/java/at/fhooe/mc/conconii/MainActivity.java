1a1ee18f2cbe1d6c34140fbfe72af0b335487af7
Christopher Ebner <giix09@gmail.com>
2015-06-20 17:35:45 +0200

    spinner implemented ;DDDDD

--- Conconii/app/src/main/java/at/fhooe/mc/conconii/MainActivity.java
+++ Conconii/app/src/main/java/at/fhooe/mc/conconii/MainActivity.java
@@ -1,12 +1,19 @@
 package at.fhooe.mc.conconii;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -17,7 +24,7 @@ public class MainActivity extends Activity {
     private static final int STORE_PERIOD = 50; //interval for storing the data in meters
     public static boolean testFinished = false;
     private float mDistance = 0; //the actual distance since the start in meters
-    private boolean mImageSet=true;
+    private boolean mImageSet = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,26 +32,50 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         instance = this;
         DataManager.getInstance(); //initial Singleton creation
+
+        View startscreenView = findViewById(R.id.startscreen_backLayout_vertical);
+
+        // hide the quit button
+        Button quitButton = (Button) findViewById(R.id.mainActivity_button_quit);
+        quitButton.setVisibility(View.GONE);
+
+        //startscreen spinner, seek and button
+
+        //spinner with the device names of the scanned dvices (from DataManager - scannedDevices ArrayList<BluetoothDevice>)
+        Spinner sensorSpinner = (Spinner) findViewById(R.id.startscreen_heartRateSensors_spinner);
+        ArrayList<String> deviceNames = new ArrayList<>();
+
+        for (int i = 0; i < DataManager.getInstance().getScannedDevices().toArray().length; i++) {
+            BluetoothDevice device = (BluetoothDevice) DataManager.getInstance().getScannedDevices().get(i);
+            deviceNames.add(device.getName());
+        }
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, deviceNames);
+        sensorSpinner.setAdapter(spinnerAdapter);
+        sensorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                BluetoothDevice chosenDevice = (BluetoothDevice) DataManager.getInstance().getScannedDevices().remove(position);
+                DataManager.getInstance().getScannedDevices().add(0, chosenDevice);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        SeekBar startSpeedSeekBar = (SeekBar) findViewById(R.id.startscreen_startspeed_seekBar);
+        Button startButton = (Button) findViewById(R.id.startscreen_button_start);
+
+        //TODO: only for testing purposes
         startService(new Intent(getApplicationContext(), GpsService.class));
         startService(new Intent(getApplicationContext(), BluetoothService.class));
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         //TODO: start fragment and put button logic into it
-
-
-        Button b = (Button) findViewById(R.id.mainActivity_button_quit);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                testFinished = true;
-                mDistance = 0;//only for testing
-                System.runFinalization();
-                System.exit(0);
-            }
-        });
     }
 
 
@@ -89,10 +120,10 @@ public class MainActivity extends Activity {
         ImageView v = (ImageView) findViewById(R.id.mainActivity_image_HeartRate);
         if (mImageSet) {
             v.setImageResource(R.drawable.ic_favorite_border_black_48dp);
-            mImageSet=false;
+            mImageSet = false;
         } else {
             v.setImageResource(R.drawable.ic_favorite_48pt_3x);
-            mImageSet=true;
+            mImageSet = true;
         }
     }
 
